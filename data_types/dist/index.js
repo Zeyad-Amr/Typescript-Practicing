001df2a283ac7c7b4cfdaf544ff4589468fa689b
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-03-10 17:09:56 +0200

    data types

--- data_types/dist/index.js
+++ data_types/dist/index.js
@@ -1,2 +1,110 @@
 "use strict";
+console.log('\n########################## Typescript Type Aliases');
+let name1 = 'John';
+name1 = 'Jane';
+let age = 20;
+age = '20';
+age = 20;
+console.log('\n########################## Typescript Advanced Type Aliases');
+function getAction(buttons) {
+    console.log(`Up: ${buttons.up}, Down: ${buttons.down}, Left: ${buttons.left}, Right: ${buttons.right}`);
+}
+let buttons = {
+    up: 'move up',
+    down: 'move down',
+    left: 'move left',
+    right: 'move right'
+};
+getAction(buttons);
+function getAction2(buttons) {
+    console.log(`Up: ${buttons.up}, Down: ${buttons.down}, Left: ${buttons.left}, Right: ${buttons.right}, Last: ${buttons.last}`);
+}
+let buttons2 = {
+    up: 'move up',
+    down: 'move down',
+    left: 'move left',
+    right: 'move right',
+    last: true
+};
+getAction2(buttons2);
+console.log('\n########################## Typescript Literal Types');
+function compare(a, b) {
+    if (a === b) {
+        return 0;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return -1;
+}
+console.log(compare(1, 1));
+console.log(compare(1, 0));
+console.log(compare(0, 1));
+console.log('\n########################## Typescript Tuple Types');
+let person = ['John', 20, true];
+person = ['Jane', 20, true];
+console.log(person);
+console.log(person);
+const [p_name, p_age, p_isMarried] = person;
+console.log(p_name, p_age, p_isMarried);
+console.log('\n########################## Typescript Void and Never Types');
+function logging(message) {
+    console.log(message);
+    return;
+}
+console.log(logging('Hello World'));
+const neverFunc = (message) => {
+    throw new Error(message);
+};
+const neverFunc2 = (message) => {
+    while (true) {
+        console.log(message);
+    }
+};
+console.log('\n########################## Typescript Type Enums');
+const easy = 0;
+const medium = 1;
+const hard = 2;
+const Multiplier = (level) => level * 10;
+var Kids;
+(function (Kids) {
+    Kids[Kids["kid"] = 0] = "kid";
+    Kids[Kids["teen"] = 1] = "teen";
+    Kids[Kids["adult"] = 2] = "adult";
+    Kids[Kids["s"] = 3] = "s";
+})(Kids || (Kids = {}));
+var Levels;
+(function (Levels) {
+    Levels[Levels["inmpossible"] = 0] = "inmpossible";
+    Levels[Levels["easy"] = 0] = "easy";
+    Levels[Levels["medium"] = 1] = "medium";
+    Levels[Levels["hard"] = 2] = "hard";
+    Levels[Levels["veryHard"] = Multiplier(Levels.medium)] = "veryHard";
+})(Levels || (Levels = {}));
+;
+let lvl = Levels.veryHard;
+if (lvl === Levels.veryHard) {
+    console.log(`Level is ${lvl}`);
+}
+console.log('\n########################## Typescript Type Assertions');
+let data = "1000";
+console.log(data.repeat(3));
+console.log('\n########################## Typescript Union and Intersection Types');
+let ccc = {
+    one: 'one',
+    two: 'two',
+    three: 'three'
+};
+let ddd = {
+    three: 'three'
+};
+let ddd2 = {
+    one: 'one',
+    two: 'two',
+    three: 'three'
+};
+let ddd3 = {
+    one: 'one',
+    two: 'two',
+};
 //# sourceMappingURL=index.js.map
--- data_types/index.ts
+++ data_types/index.ts
@@ -1,2 +1,255 @@
-console.log('\n######################### Typescript Type Aliases');
+// ######################################################################################################
+console.log('\n########################## Typescript Type Aliases');
+
+type st= string;
+
+let name1: st = 'John';
+name1 = 'Jane';
+// name1 = 50; // error
+
+type stringOrNumber = string | number;
+
+let age: stringOrNumber = 20;
+age = '20';
+age = 20;
+// age = true; // error
+
+
+// ######################################################################################################
+console.log('\n########################## Typescript Advanced Type Aliases');
+// Advanced Types Aliases in Typescript
+type Buttons = {
+    up: string,
+    down: string,
+    left: string,
+    right: string
+}
+
+function getAction(buttons: Buttons) {
+    console.log(`Up: ${buttons.up}, Down: ${buttons.down}, Left: ${buttons.left}, Right: ${buttons.right}`);
+}
+
+let buttons = {
+    up: 'move up',
+    down: 'move down',
+    left: 'move left',
+    right: 'move right'
+}
+
+getAction(buttons);
+
+// extending type aliases
+
+type Last =Buttons&{last: boolean};
+
+function getAction2(buttons: Last) {
+    console.log(`Up: ${buttons.up}, Down: ${buttons.down}, Left: ${buttons.left}, Right: ${buttons.right}, Last: ${buttons.last}`);
+}
+
+let buttons2 = {    
+    up: 'move up',
+    down: 'move down',
+    left: 'move left',
+    right: 'move right',
+    last: true
+}
+
+getAction2(buttons2);
+
+
+// ######################################################################################################
+console.log('\n########################## Typescript Literal Types');
+// Literal Types in Typescript
+
+type nums = -1|0|1;
+
+function compare(a: nums, b: nums) :nums{
+    if (a === b) {
+        return 0;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return -1;
+}
+
+console.log(compare(1, 1));
+console.log(compare(1, 0));
+console.log(compare(0, 1));
+
+// ######################################################################################################
+console.log('\n########################## Typescript Tuple Types');
+// Tuple Types in Typescript
+/*
+- Tuple 
+--- Is Another way to represent an array
+--- Each element in the tuple has a type and position
+--- The position is fixed
+--- The length is fixed
+--- The type is fixed
+
+*/
+
+type Person = readonly [string, number, boolean];
+
+let person: Person = ['John', 20, true];
+person= ['Jane', 20, true];
+
+console.log(person);
+
+// person[0] = 'Jane'; // error
+
+console.log(person);
+
+// destructuring
+const [p_name, p_age, p_isMarried] = person;
+console.log(p_name, p_age, p_isMarried);
+
+
+// ######################################################################################################
+console.log('\n########################## Typescript Void and Never Types');
+/*
+- Void
+--- Used to represent the absence of a value
+--- Function that void can only return undefined
+*/
+
+function logging(message: string):void {
+    console.log(message);
+    return;
+}
+
+console.log(logging('Hello World'));
+
+/*
+- never 
+--- function that never returns
+*/
+
+const neverFunc = (message: string) => {
+    throw new Error(message);
+    // return 10;
+}
+
+const neverFunc2 = (message: string):never => {
+    while(true) {
+        console.log(message);
+    }
+    // return 10; // error
+}
+
+
+// ######################################################################################################
+console.log('\n########################## Typescript Type Enums');
+
+/*
+- Enums: Enumeration
+--- Used to define a set of named constants
+--- Each constant has a name and a value
+--- The value can be a number or a string
+--- By Default, the value is the index of the constant
+--- The index starts from 0
+--- The value can be changed
+--- Theres a string based enum and a numeric based enum
+
+*/
+
+const easy = 0;
+const medium = 1;
+const hard = 2;
+const Multiplier=(level: number) => level*10;
+
+enum Kids {
+    kid=0,
+    teen=1,
+    adult=2,
+    s
+}
+
+enum Levels{
+    inmpossible,
+    easy=Kids.kid, 
+    medium=Kids.teen, 
+    hard=Kids.adult,
+    veryHard=Multiplier(medium),
+    // impossible=Multiplier(veryHard) // error
+};
+
+let lvl = Levels.veryHard;
+if (lvl === Levels.veryHard) {
+    console.log(`Level is ${lvl}`);
+}
+
+
+// ######################################################################################################
+console.log('\n########################## Typescript Type Assertions');
+/*
+- Type Assertions
+--- Used to override the type inference
+--- Used to tell the compiler that we know better
+--- TypeScript will not perform any type checking after the assertion
+*/
+
+
+// let myImg = document.querySelector('img') as HTMLImageElement;
+// let myImg =<HTMLImageElement> document.querySelector('img');
+
+// console.log(myImg.src);
+// console.log(myImg.value); // error
+
+
+// let data:any = 1000; // error
+let data:any = "1000";
+console.log((data as string).repeat(3));
+
+
+// ######################################################################################################
+console.log('\n########################## Typescript Union and Intersection Types');
+
+/*
+- Union Types
+--- | symbol is used to define a union type => "Or"
+
+- Intersection Types
+--- & symbol is used to define a intersection type => "And"
+
+--- If a union is OR then intersection is AND
+*/
+
+type A={
+    one: string,
+    two: string
+
+}
+
+type B={
+    three: string,
+}
+
+type C= A&B;
+
+type D= A|B;
+
+let ccc: C = {
+    one: 'one',
+    two: 'two',
+    three: 'three'
+}
+
+let ddd: D = {
+ 
+    three: 'three'
+}
+
+let ddd2: D = {
+    one: 'one',
+    two: 'two',
+    three: 'three'
+}
+
+let ddd3: D = {
+    one: 'one',
+    two: 'two',
+}
+
 
